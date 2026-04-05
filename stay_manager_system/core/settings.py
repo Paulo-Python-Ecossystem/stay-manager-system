ef8067693dfa5d7523c04b5b6bf56376e1b82846
Paulo Moreira <prmoreiraf.dev@gmail.com>
2026-04-05 16:27:18 -0300

    #FIX: Adjusts codequality

--- stay_manager_system/core/settings.py
+++ stay_manager_system/core/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-i0a21tki03q5-v_ft#)74apzvt37hx+s)0m(gat!aks_dp+3fg"  # noqa: B105
+SECRET_KEY = "django-insecure-i0a21tki03q5-v_ft#)74apzvt37hx+s)0m(gat!aks_dp+3fg"  # nosec B105
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
